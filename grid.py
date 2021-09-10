c03cb8ca10b243a5859c390ca28904b035ac0a1c
ahmed4end <as4end@gmail.com>
2021-09-10 14:39:16 +0200

    init 2

--- grid.py
+++ grid.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageDraw
-from core import Data
+import random 
 
 A4 = (2480, 3508)
 
 class Grid:
 
-	def __init__(self, cols:int, rows:int, width:int=2480, height:int=3508, *args, **kwargs):
+	def __init__(self,  rows:int, cols:int, width:int=2480, height:int=3508, *args, **kwargs):
 		'''
 			:kwargs:
 				bgcolor: image background color
@@ -13,7 +13,9 @@ class Grid:
 				border_width: border width
 				hlines: horizontal lines drawing bool
 				vlines: vertical lines drawing bool
+				border: border bool
 				margin: (top,right,bottom,left) or integer
+				cell_margin: cell margin to all sides.
 		'''
 		self.cols = cols
 		self.rows = rows
@@ -22,54 +24,100 @@ class Grid:
 		self.image = Image.new(mode='RGBA', size=(width, height), color=kwargs.get('bgcolor',(0,0,0,0)))
 		self.draw = ImageDraw.Draw(self.image)
 		self.kwargs = kwargs
+		self.border_width = kwargs.get('border_width',1)
+		self.cell_margin = kwargs.get('cell_margin',1)
 		self.margin = self._margin()
-		self.border_width = kwargs.get('border_width')
+		
 		self.grid_metrics = self._grid_metrics()
 		
 
 	def _grid_metrics(self,):
-		row_piece_val = (self.height-self.margin[0]-self.margin[2]-self.border_width)//self.rows
-		col_piece_val = (self.width-self.margin[1]-self.margin[3]-self.border_width)//self.cols
-		return Data(
-			row_width= row_piece_val, 
-			col_width= col_piece_val,
-		)
+		row_piece_val = (self.height-self.margin[0]-self.margin[2])//self.rows
+		col_piece_val = (self.width-self.margin[1]-self.margin[3])//self.cols
+		return {
+			'row_height': row_piece_val,
+			'col_height': col_piece_val
+		}
 
 	def _margin(self):
 		margin = self.kwargs.get('margin')
 		if isinstance(margin,tuple) and len(margin)==4 and all(map(int,margin)):
-			return margin
+			return tuple(map(lambda x: x+self.border_width//2,margin))
 		elif isinstance(margin,int):
-			return (margin,)*4
+			return tuple(map(lambda x: x+self.border_width//2,(margin,)*4))
 		else:
-			return (0,)*4
+			return (self.border_width//2,)*4
 
 	def draw_lines(self):
 		_h, _w = self.margin[0], self.margin[1]
 		if self.kwargs.get('hlines') or 'hlines' not in self.kwargs:
-			for row in range(self.rows+1):
+			for row in range(self.rows-1):
+				_h += self.grid_metrics['row_height']
 				self.draw.line(
 					xy=[self.width-self.margin[1], _h, self.margin[3], _h], 
 					fill=self.kwargs.get('border_color','black'), 
 					width=self.border_width
-				)
-				_h += self.grid_metrics['row_width']
+				)	
 		
 		if self.kwargs.get('vlines') or 'vlines' not in self.kwargs:
-			for col in range(self.cols+1):
-				self.draw.line(xy=[self.width-_w-self.border_width, self.margin[0], self.width-_w-self.border_width, self.height-self.margin[2]], 
+			for col in range(self.cols-1):
+				_w += self.grid_metrics['col_height']
+				self.draw.line(xy=[self.width-_w, self.margin[0], self.width-_w, self.height-self.margin[2]], 
 					fill=self.kwargs.get('border_color','black'), 
 					width=self.border_width
 				)
-				_w += self.grid_metrics['col_width']
+		
+		if self.kwargs.get('border') or 'border' not in self.kwargs:
+			self.draw.rectangle(
+				xy=(self.margin[3]-self.border_width//2, 
+					self.margin[0]-self.border_width//2, 
+					A4[0]-self.margin[2]+self.border_width//2, 
+					A4[1]-self.margin[1]+self.border_width//2),
+				fill=None,
+				outline=self.kwargs.get('border_color','black'), 
+				width=self.border_width)
 
+	@property
 	def cells(self):
-		pass
+		_cells = []
+		_col_height = self.grid_metrics['row_height']
+		_col_width = self.grid_metrics['col_height']
+		for row in range(self.rows):
+			_row = []
+			for col in range(self.cols):
 
+				# cell xy
+				_cell = (_col_width*col+self.margin[3]+self.border_width//2+self.cell_margin,
+					_col_height*row+self.margin[0]+self.border_width//2+self.cell_margin,
+					_col_width*(col+1)+self.margin[3]-self.border_width//2-self.cell_margin,
+					_col_height*(row+1)+self.margin[0]-self.border_width//2-self.cell_margin
+				)
+
+				_row.append(_cell)
+
+			_cells.append(_row)
+
+		# reverse row order to RTL
+		_cells = list(map(lambda x: x[::-1], _cells)) 	
+
+		return _cells
+
+	def rndcolor(self):
+		return (random.randint(50,200), random.randint(50,200), random.randint(50,200))
+
+	def color_cells(self):
+		for row in self.cells:
+			for col in row:
+				self.draw.rectangle(xy=col,fill=self.rndcolor())
 
 if __name__=='__main__':
-	obj = Grid(4,4, bgcolor='lightgrey', border_color='red', margin=100, border_width=5)
+	from pprint import pprint
+	obj = Grid(20,4, border_width=25, cell_margin=25, margin=50, bgcolor='lightgrey')
 	details = obj.grid_metrics
 	print(details)
 	obj.draw_lines()
+	obj.color_cells()
+	pprint(obj.cells)
 	obj.image.show()
+	#obj.image.save('example.png')
+	
